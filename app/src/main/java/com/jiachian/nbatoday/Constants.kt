5b04a9def8b6c670a2709dc857d5c57aa3fe3e8f
杜佳謙 <allen.du@kdanmobile.com>
2022-12-15 17:56:33 +0800

    M: improve cookie handler

--- app/src/main/java/com/jiachian/nbatoday/Constants.kt
+++ app/src/main/java/com/jiachian/nbatoday/Constants.kt
@@ -1,7 +1,7 @@
 package com.jiachian.nbatoday
 
 const val CDN_BASE_URL = "https://cdn.nba.com/"
-const val STATS_BASE_URL = "https://stats.nba.com/"
+const val STATS_BASE_URL = "https://stats.nba.com/stats/"
 
 const val SCHEDULE_DATE_RANGE = 7
 
