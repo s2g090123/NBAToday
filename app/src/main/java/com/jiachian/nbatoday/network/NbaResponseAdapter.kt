2092498bdaeb804854e995dbb9c94567741a519d
s2g090123 <chian980602@gmail.com>
2023-08-02 11:58:53 +0800

    Update: Excluding NetworkResponse from JaCoCo report

--- app/src/main/java/com/jiachian/nbatoday/network/NbaResponseAdapter.kt
+++ app/src/main/java/com/jiachian/nbatoday/network/NbaResponseAdapter.kt
@@ -1,9 +1,11 @@
 package com.jiachian.nbatoday.network
 
+import com.jiachian.nbatoday.annotation.ExcludeFromJacocoGeneratedReport
+import java.lang.reflect.Type
 import retrofit2.Call
 import retrofit2.CallAdapter
-import java.lang.reflect.Type
 
+@ExcludeFromJacocoGeneratedReport
 class NbaResponseAdapter<S : Any>(
     private val resultType: Type
 ) : CallAdapter<S, Call<NbaResponse<S>>> {
--- app/src/main/java/com/jiachian/nbatoday/network/NbaResponseCall.kt
+++ app/src/main/java/com/jiachian/nbatoday/network/NbaResponseCall.kt
@@ -1,18 +1,21 @@
 package com.jiachian.nbatoday.network
 
+import com.jiachian.nbatoday.annotation.ExcludeFromJacocoGeneratedReport
+import java.io.IOException
 import okhttp3.Request
 import okio.Timeout
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import java.io.IOException
 
+@ExcludeFromJacocoGeneratedReport
 class NbaResponseCall<S : Any>(
     private val delegate: Call<S>,
 ) : Call<NbaResponse<S>> {
 
     override fun enqueue(callback: Callback<NbaResponse<S>>) {
         return delegate.enqueue(
+            @ExcludeFromJacocoGeneratedReport
             object : Callback<S> {
                 override fun onResponse(call: Call<S>, response: Response<S>) {
                     val body = response.body()
