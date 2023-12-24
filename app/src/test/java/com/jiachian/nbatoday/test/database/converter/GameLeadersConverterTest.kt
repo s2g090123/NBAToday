3580d16370b21eb9ff39630ec3bcff5222504adc
s2g090123 <chian980602@gmail.com>
2023-12-24 18:07:26 +0800

    Fix: Setting the wrong adapter type

--- app/src/test/java/com/jiachian/nbatoday/test/database/converter/GameLeadersConverterTest.kt
+++ app/src/test/java/com/jiachian/nbatoday/test/database/converter/GameLeadersConverterTest.kt
@@ -5,7 +5,6 @@ import com.jiachian.nbatoday.data.local.GameGenerator
 import com.jiachian.nbatoday.database.converter.GameLeadersConverter
 import com.jiachian.nbatoday.database.converter.typeAdapterGson
 import com.jiachian.nbatoday.models.local.game.GameLeaders
-import com.jiachian.nbatoday.models.local.score.BoxScore
 import com.jiachian.nbatoday.utils.assertIs
 import org.junit.Test
 
@@ -26,7 +25,7 @@ class GameLeadersConverterTest {
     fun `to(final) expects correct`() {
         val leaders = GameGenerator.getFinal().gameLeaders
         val gson = typeAdapterGson
-        val type = object : TypeToken<BoxScore.BoxScoreTeam>() {}.type
+        val type = object : TypeToken<GameLeaders>() {}.type
         val actual = converter.to(gson.toJson(leaders, type))
         assertIs(actual, leaders)
     }
