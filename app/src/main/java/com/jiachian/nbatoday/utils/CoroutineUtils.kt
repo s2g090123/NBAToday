99dcd2454b4f05473fbb759f89a442ad55a7bd57
s2g090123 <chian980602@gmail.com>
2024-03-30 18:43:00 +0800

    Update: Remove unused files and functions

--- app/src/main/java/com/jiachian/nbatoday/utils/CoroutineUtils.kt
+++
@@ -1,5 +0,0 @@
-package com.jiachian.nbatoday.utils
-
-import kotlinx.coroutines.flow.SharingStarted
-
-val WhileSubscribed5000 = SharingStarted.WhileSubscribed(5000)
--- app/src/main/java/com/jiachian/nbatoday/utils/ToastUtils.kt
+++ app/src/main/java/com/jiachian/nbatoday/utils/ToastUtils.kt
@@ -1,29 +1,9 @@
 package com.jiachian.nbatoday.utils
 
 import android.content.Context
-import android.os.Handler
-import android.os.Looper
 import android.widget.Toast
 import androidx.annotation.StringRes
 import com.jiachian.nbatoday.MainApplication
-import com.jiachian.nbatoday.R
-
-fun showErrorToast() {
-    val showToast = {
-        val context = MainApplication.context
-        showToast(
-            context,
-            R.string.error
-        )
-    }
-    if (Looper.myLooper() != Looper.getMainLooper()) {
-        Handler(Looper.getMainLooper()).post {
-            showToast()
-        }
-    } else {
-        showToast()
-    }
-}
 
 fun showToast(
     @StringRes stringRes: Int,
