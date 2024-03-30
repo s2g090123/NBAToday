30e1d2843e0b481a0ca2e2d6f957b1fe7520b8c3
s2g090123 <chian980602@gmail.com>
2024-03-30 17:35:46 +0800

    Update: Refactor GetBetGames

--- app/src/main/java/com/jiachian/nbatoday/usecase/bet/GetBetGames.kt
+++ app/src/main/java/com/jiachian/nbatoday/usecase/bet/GetBetGames.kt
@@ -2,15 +2,17 @@ package com.jiachian.nbatoday.usecase.bet
 
 import com.jiachian.nbatoday.models.local.bet.BetAndGame
 import com.jiachian.nbatoday.repository.bet.BetRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.mapLatest
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class GetBetGames(
     private val repository: BetRepository,
 ) {
     operator fun invoke(account: String): Flow<List<BetAndGame>> {
         return repository.getBetGames(account)
-            .map { games ->
+            .mapLatest { games ->
                 games.sortedBy { game ->
                     game.game.gameDateTime
                 }
