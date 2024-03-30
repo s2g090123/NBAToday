257219bab57234879839f4708785924ef38950c1
s2g090123 <chian980602@gmail.com>
2024-03-30 17:27:02 +0800

    Update: Remove unused functions

--- app/src/main/java/com/jiachian/nbatoday/common/Response.kt
+++ app/src/main/java/com/jiachian/nbatoday/common/Response.kt
@@ -3,16 +3,4 @@ package com.jiachian.nbatoday.common
 sealed class Response<T> {
     data class Success<T>(val data: T) : Response<T>()
     data class Error<T>(val message: String? = null) : Response<T>()
-
-    fun onSuccess(invoke: (T) -> Unit) {
-        if (this is Success) {
-            invoke(data)
-        }
-    }
-
-    fun onError(invoke: (String?) -> Unit) {
-        if (this is Error) {
-            invoke(message)
-        }
-    }
 }
