e9d31601b5237054dcec7857c69694338951843c
s2g090123 <chian980602@gmail.com>
2024-03-30 16:43:49 +0800

    Update: Delete unnecessary dispatcher switching

--- app/src/main/java/com/jiachian/nbatoday/usecase/game/GetGame.kt
+++ app/src/main/java/com/jiachian/nbatoday/usecase/game/GetGame.kt
@@ -1,15 +1,12 @@
 package com.jiachian.nbatoday.usecase.game
 
-import com.jiachian.nbatoday.dispatcher.DefaultDispatcherProvider
-import com.jiachian.nbatoday.dispatcher.DispatcherProvider
+import com.jiachian.nbatoday.models.local.game.GameAndBets
 import com.jiachian.nbatoday.repository.game.GameRepository
-import kotlinx.coroutines.withContext
 
 class GetGame(
     private val repository: GameRepository,
-    private val dispatcherProvider: DispatcherProvider = DefaultDispatcherProvider,
 ) {
-    suspend operator fun invoke(gameId: String) = withContext(dispatcherProvider.io) {
-        repository.getGameAndBets(gameId)
+    suspend operator fun invoke(gameId: String): GameAndBets {
+        return repository.getGameAndBets(gameId)
     }
 }
