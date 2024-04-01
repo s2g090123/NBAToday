4f343a8e4a9611b9c3d7ab5d40953603a6be3f38
s2g090123 <chian980602@gmail.com>
2024-04-01 17:51:41 +0800

    Update: Refactor by detekt SwallowedException

--- app/src/main/java/com/jiachian/nbatoday/player/domain/AddPlayer.kt
+++ app/src/main/java/com/jiachian/nbatoday/player/domain/AddPlayer.kt
@@ -12,13 +12,9 @@ class AddPlayer(
 ) {
     suspend operator fun invoke(playerId: Int): Flow<Resource<Unit, AddPlayerError>> = flow {
         emit(Resource.Loading())
-        try {
-            when (repository.addPlayer(playerId)) {
-                is Response.Error -> emit(Resource.Error(AddPlayerError.ADD_FAILED))
-                is Response.Success -> emit(Resource.Success(Unit))
-            }
-        } catch (e: Exception) {
-            emit(Resource.Error(AddPlayerError.ADD_FAILED))
+        when (repository.addPlayer(playerId)) {
+            is Response.Error -> emit(Resource.Error(AddPlayerError.ADD_FAILED))
+            is Response.Success -> emit(Resource.Success(Unit))
         }
     }
 }
